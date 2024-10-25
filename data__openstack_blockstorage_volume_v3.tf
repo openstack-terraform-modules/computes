99f1cfdfadf673ef1dbf6983dd7f0ee0a2d61bf1
tannevaled <tannevaled@users.noreply.github.com>
2024-10-25 15:49:35 +0200

    wip

--- data__openstack_blockstorage_volume_v3.tf
+++ data__openstack_blockstorage_volume_v3.tf
@@ -1,4 +1,4 @@
-data openstack_blockstorage_volume_v3 data {
+data openstack_blockstorage_volume_v3 volume {
     metadata = {
         GROUP_PREFIX = local.group_prefix
     }
--- resources__openstack_compute_volume_attach_v2.tf
+++ resources__openstack_compute_volume_attach_v2.tf
@@ -1,5 +1,5 @@
 resource openstack_compute_volume_attach_v2 data {
-  for_each = local.volumes
+  for_each = openstack_blockstorage_volume_v3.*.volume
   #count = local.count
   instance_id = openstack_compute_instance_v2.compute[each.key].id
   volume_id   = each.value
