b3d6d1dc9bdbbc90657260f71209e8d330a249e2
tannevaled <tannevaled@users.noreply.github.com>
2024-10-25 15:46:06 +0200

    wip

--- terraform__locals.tf
+++ terraform__locals.tf
@@ -2,5 +2,7 @@ locals {
     //volumes = lookup(var.filter-volumes, var.group_prefix).volumes
     //count = length(local.volumes)
     //cluster_prefix = var.get-volumes.cluster_prefix
+    cluster_prefix = var.cluster_prefix
+    group_prefix = var.group_prefix
     flavor_name = var.flavor_name
 }
