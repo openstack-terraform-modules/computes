d3f06bc288ac0ba4765c0b444a64e2794dfb0f52
tannevaled <tannevaled@users.noreply.github.com>
2024-10-25 14:30:51 +0200

    wip

--- resources__openstack_compute_volume_attach_v2.tf
+++ resources__openstack_compute_volume_attach_v2.tf
@@ -1,8 +1,5 @@
 resource openstack_compute_volume_attach_v2 data {
   count = local.count
-  instance_id = openstack_compute_instance_v2.compute.id
-  volume_id   = element(
-    local.volumes,
-    count.index
-  )
+  instance_id = openstack_compute_instance_v2.compute[count.index].id
+  volume_id   = element(local.volumes, count.index)
 }
