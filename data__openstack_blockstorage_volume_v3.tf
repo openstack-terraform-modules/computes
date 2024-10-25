5f7adbd4b11869a484ca4b4cd324b32fcef90834
tannevaled <tannevaled@users.noreply.github.com>
2024-10-25 16:36:21 +0200

    wip

--- data__openstack_blockstorage_volume_v3.tf
+++ data__openstack_blockstorage_volume_v3.tf
@@ -1,7 +1,8 @@
 data openstack_blockstorage_volume_v3 volume {
     count = local.compute_count
     metadata = {
-        GROUP_PREFIX = local.group_prefix
-        GROUP_INDEX = count.index+1
+        "DEPLOYMENT_UUID": local.deployement.uuid,
+        "GROUP_PREFIX": local.group_prefix,
+        "GROUP_INDEX": count.index+1
     }
 }
