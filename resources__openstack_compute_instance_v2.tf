c9a736559784725bf969afa1d5d707bef1710277
tannevaled <tannevaled@users.noreply.github.com>
2024-10-25 16:41:19 +0200

    wip

--- resources__openstack_compute_instance_v2.tf
+++ resources__openstack_compute_instance_v2.tf
@@ -1,10 +1,10 @@
 resource openstack_compute_instance_v2 compute {
-    for_each = data.openstack_blockstorage_volume_v3.volume
+    count = local.compute_count
     #local.count
 
     name = join("-", [
         local.cluster_prefix,
-        each.key
+        count.index+1
     ])
 
     flavor_name = local.flavor_name
--- resources__openstack_compute_volume_attach_v2.tf
+++ resources__openstack_compute_volume_attach_v2.tf
@@ -1,7 +1,5 @@
 resource openstack_compute_volume_attach_v2 data {
-  for_each = data.openstack_blockstorage_volume_v3.volume
-  #count = local.count
-  instance_id = openstack_compute_instance_v2.compute[each.key].id
-  volume_id   = each.value.id
-  #element(local.volumes, count.index)
+  count = local.compute_count
+  instance_id = openstack_compute_instance_v2.compute[count.index].id
+  volume_id   = data.openstack_blockstorage_volume_v3.volume[count.index].id
 }
