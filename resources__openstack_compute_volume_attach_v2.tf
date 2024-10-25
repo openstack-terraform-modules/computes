464bdeab327b022d23cf50c8e827ea908333216d
tannevaled <tannevaled@users.noreply.github.com>
2024-10-25 15:52:11 +0200

    wip

--- resources__openstack_compute_volume_attach_v2.tf
+++ resources__openstack_compute_volume_attach_v2.tf
@@ -1,7 +1,7 @@
 resource openstack_compute_volume_attach_v2 data {
-  for_each = openstack_blockstorage_volume_v3.volume
+  for_each = data.openstack_blockstorage_volume_v3.volume
   #count = local.count
   instance_id = openstack_compute_instance_v2.compute[each.key].id
-  volume_id   = each.value
+  volume_id   = each.value.id
   #element(local.volumes, count.index)
 }
